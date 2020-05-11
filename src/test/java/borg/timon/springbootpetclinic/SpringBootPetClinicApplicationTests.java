36cc664b3d66258953b7c389b745edf86e93f805
Timon Borg <tborg@bol.com>
2020-05-11 16:46:44 +0200

    Added spring boot

---
+++ src/test/java/borg/timon/springbootpetclinic/SpringBootPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package borg.timon.springbootpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
